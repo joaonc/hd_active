f883743e39de8ccec8a435d0a2774e7f3e56d313
Joao Coelho <joaonc@hotmail.com>
2022-01-02 20:46:34 -0600

    Removing license from every file. One in the project should be enough.

--- hd_active/app.py
+++ hd_active/app.py
@@ -1,24 +1,9 @@
-# Copyright 2022 Joao Coelho - MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-# persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-# Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 import sys
 
-from PySide6 import QtWidgets, QtGui
+from PySide6 import QtGui, QtWidgets
+from utils import get_asset
 
 from hd_active import HdActive
-from utils import get_asset
 
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
@@ -49,4 +34,4 @@ def main(drive_paths=None, run=True, wait=1):
 
 
 if __name__ == '__main__':
-    main(drive_paths=['e:\\_hd_active.txt'])
+    main(drive_paths=['e:\\'])
--- hd_active/hd_active.py
+++ hd_active/hd_active.py
@@ -1,46 +1,36 @@
-# Copyright 2022 Joao Coelho - MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-# persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-# Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 import threading
 import time
 from pathlib import Path
+from typing import Optional, Union
 
-FILE_NAME = '_HD_ACTIVE.TXT'
+FILE_NAME = '_hd_active.txt'
 
 
 class HdActive:
-    def __init__(self, drive_paths=None, run=False, wait=1):
+    def __init__(self, drive_paths=Optional[list], run: bool = False, wait: Union[int, float] = 1):
         self.drive_paths = drive_paths or []
         self._is_running = False
         self.wait = wait
         if run:
             self.start()
 
-    def write_hd(self) -> None:
+    @staticmethod
+    def _write_hd(drive_path: Path) -> None:
+        file_path = Path(drive_path)
+        if file_path.is_dir():
+            file_path /= FILE_NAME
+        if not file_path.parent.exists():
+            raise FileNotFoundError(file_path)
+        file_path.open('w').write(str(time.time()))
+        file_path.unlink()
+
+    def write_hds(self) -> None:
         for drive_path in self.drive_paths:
-            file_path = Path(drive_path)
-            if file_path.is_dir():
-                file_path /= FILE_NAME
-            if not file_path.parent.exists():
-                raise FileNotFoundError(file_path)
-            file_path.open('w').write(str(time.time()))
-            file_path.unlink()
+            self._write_hd(drive_path)
 
     def _do_write_hd(self):
         while self.is_running:
-            self.write_hd()
+            self.write_hds()
             time.sleep(self.wait)
 
     @property
