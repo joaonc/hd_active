214bc2486e669fc8cf6a7fd2f813897ebbf65149
Joao Coelho <joaonc@hotmail.com>
2022-01-03 07:21:40 -0600

    Adding `is_truthy()`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from hd_active.utils import get_asset
+from hd_active.utils import get_asset, is_truthy
 
 
 class TestGetAsset:
@@ -8,3 +8,18 @@ class TestGetAsset:
     def test_exists(self, path):
         asset = get_asset(path)
         assert asset.exists()
+
+
+class TestIsTruthy:
+    @pytest.mark.parametrize('value', [True, 'True', 'true', 'YeS', 'ON', 1, 1.1, -1])
+    def test_true(self, value):
+        assert is_truthy(value) is True
+
+    @pytest.mark.parametrize('value', [False, 'False', 'false', 'NO', 'OfF', 0, 0.0])
+    def test_false(self, value):
+        assert is_truthy(value) is False
+
+    @pytest.mark.parametrize('value', ['foo', object()])
+    def test_undetermined(self, value):
+        with pytest.raises(ValueError):
+            is_truthy(value)
