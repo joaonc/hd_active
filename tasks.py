89db15b8ad366d24af44f802a438f2975a40d11e
Joao Coelho <joaonc@users.noreply.github.com>
2022-01-26 21:55:46 -0600

    Tasks namespaces (#29)

--- tasks.py
+++ tasks.py
@@ -1,24 +1,65 @@
 import os
 from pathlib import Path
 
-from invoke import task
+from invoke import Collection, task
 
 from app.utils import get_asset
 
 PROJECT_DIR = Path()
 UI_FILES = tuple(get_asset('ui').glob("**/*.ui"))
+"""
+QT ``.ui`` files.
+"""
+REQUIREMENTS_MAIN = 'main'
+REQUIREMENTS_FILES = {
+    REQUIREMENTS_MAIN: 'requirements.txt',
+    'dev': 'dev-requirements.txt',
+    'docs': 'docs-requirements.txt',
+}
+"""
+Requirements files.
+Order matters as most operations with multiple files need ``requirements.txt`` to be processed first.
+"""
+REQUIREMENTS_TASK_HELP = {
+    'requirements': '`.in` file. Full name not required, just the initial name before the dash (ex. \'dev\'). '
+    f'For main file use \'{REQUIREMENTS_MAIN}\'. Available requirements: {", ".join(REQUIREMENTS_FILES)}.'
+}
+
 os.environ.setdefault('INVOKE_RUN_ECHO', '1')  # Show commands by default
+
+
+def _csstr_to_list(csstr: str) -> list[str]:
+    return [s.strip() for s in csstr.split(',')]
+
+
+def _get_requirements_file(requirements: str) -> str:
+    filename = REQUIREMENTS_FILES.get(requirements, requirements)
+    if filename not in REQUIREMENTS_FILES.values():
+        raise FileNotFoundError(f'`{requirements}` is an unknown requirements file.')
+
+    return filename
+
+
+def _get_requirements_files(requirements: str | None) -> list[str]:
+    if requirements is None:
+        filenames = list(REQUIREMENTS_FILES.values())
+    else:
+        filenames = [_get_requirements_file(r) for r in _csstr_to_list(requirements)]
+        # Sort by the order defined in `REQUIREMENTS_FILES`
+        filenames = [f for f in REQUIREMENTS_FILES.values() if f in filenames]
+
+    return filenames
 
 
 @task(
     help={
-        'file': '`.ui` file to be converted to `.py`. If not supplied, all files will be converted. Available files: '
-        f'{", ".join(p.stem for p in UI_FILES)}'
+        'file': '`.ui` file to be converted to `.py`. `.ui` extension not required. Can be a comma separated. '
+        f'If not supplied, all files will be converted. Available files: {", ".join(p.stem for p in UI_FILES)}.'
     }
 )
-def ui(c, file=None):
+def ui_py(c, file=None):
     """
-    Convert QT `.ui` files into `.py`. `.ui` extension not required in the parameter.
+    Convert QT `.ui` files into `.py`.
     """
     if file:
         file_stems = [file[:-3] if file.lower().endswith('.ui') else file]
@@ -32,7 +73,7 @@ def ui(c, file=None):
         c.run(f'pyside6-uic {ui_file_path} -o {py_file_path}')
 
 
-@task(help={'file': f'`.ui` file to be edited. Available files: {", ".join(p.stem for p in UI_FILES)}'})
+@task(help={'file': f'`.ui` file to be edited. Available files: {", ".join(p.stem for p in UI_FILES)}.'})
 def ui_edit(c, file):
     """
     Edit a file in QT Designer.
@@ -44,16 +85,37 @@ def ui_edit(c, file):
 
 
 @task
-def lint(c):
-    """
-    Run linters (isort, black).
-    Config for each of the tools is in `pyproject.toml`.
-    """
-    c.run('isort .')
+def lint_black(c):
     c.run('black .')
+
+
+@task
+def lint_flake8(c):
     c.run('flake8 .')
+
+
+@task
+def lint_isort(c):
+    c.run('isort .')
+
+
+@task
+def lint_mypy(c):
     c.run('mypy .')
+
+
+@task
+def lint_safety(c):
     c.run('safety check')
+
+
+@task(lint_isort, lint_black, lint_flake8, lint_mypy, lint_safety)
+def lint_all(c):
+    """
+    Run all linters.
+    Config for each of the tools is in `pyproject.toml` and `setup.cfg`.
+    """
+    print('Done')
 
 
 @task
@@ -75,6 +137,68 @@ def docs_serve(c):
 @task
 def docs_deploy(c):
     """
-    Deploy documentation to GitHub.
+    Publish documentation to GitHub Pages at https://joaonc.github.io/hd_active.
     """
     c.run('mkdocs gh-deploy')
+
+
+@task(help=REQUIREMENTS_TASK_HELP)
+def pip_compile(c, requirements=None):
+    """
+    Compile requirements file.
+    """
+    for filename in _get_requirements_files(requirements):
+        c.run(f'pip-compile {filename}')
+
+
+@task(help=REQUIREMENTS_TASK_HELP)
+def pip_sync(c, requirements=None):
+    """
+    Synchronize environment with requirements file.
+    """
+    c.run(f'pip-sync {" ".join(_get_requirements_files(requirements))}')
+
+
+@task(help=REQUIREMENTS_TASK_HELP | {'package': 'Package to upgrade. Can be a comma separated list.'})
+def pip_package(c, requirements, package):
+    """
+    Upgrade package.
+    """
+    packages = [p.strip() for p in package.split(',')]
+    for filename in _get_requirements_files(requirements):
+        c.run(f'pip-compile --upgrade-package {" --upgrade-package ".join(packages)} {filename}')
+
+
+@task(help=REQUIREMENTS_TASK_HELP)
+def pip_upgrade(c, requirements):
+    """
+    Try to upgrade all dependencies to their latest versions.
+    """
+    for filename in _get_requirements_files(requirements):
+        c.run(f'pip-compile --upgrade {filename}')
+
+
+ns = Collection()  # Main namespace
+ns.add_task(test)
+docs = Collection('docs')
+docs.add_task(docs_serve, 'serve')
+docs.add_task(docs_deploy, 'deploy')
+lint = Collection('lint')
+lint.add_task(lint_all, 'all')
+lint.add_task(lint_black, 'black')
+lint.add_task(lint_flake8, 'flake8')
+lint.add_task(lint_isort, 'isort')
+lint.add_task(lint_mypy, 'mypy')
+lint.add_task(lint_safety, 'safety')
+pip = Collection('pip')
+pip.add_task(pip_compile, 'compile')
+pip.add_task(pip_package, 'package')
+pip.add_task(pip_sync, 'sync')
+pip.add_task(pip_upgrade, 'upgrade')
+ui = Collection('ui')
+ui.add_task(ui_py, 'py')
+ui.add_task(ui_edit, 'edit')
+ns.add_collection(docs)
+ns.add_collection(lint)
+ns.add_collection(pip)
+ns.add_collection(ui)
