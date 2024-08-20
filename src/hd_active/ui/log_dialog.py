f747e90d2915df6f1ba25ada5e2d1c0a751c4071
Joao Coelho <joaonc@hotmail.com>
2024-08-19 20:08:04 -0500

    refactor ui class

--- src/hd_active/ui/log_dialog.py
+++ src/hd_active/ui/log_dialog.py
@@ -1,20 +1,18 @@
 from PySide6.QtGui import QShowEvent
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QWidget
 
 from src.hd_active.hd_active import HdActive
 from src.hd_active.ui.forms.ui_log_dialog import Ui_LogDialog
 
 
-class LogDialog(QDialog):
-    # Using class composition instead of inheritance or `PySide6.QtUiTools.loadUiType`.
-    def __init__(self, hd_active: HdActive, parent):
+class LogDialog(QDialog, Ui_LogDialog):
+    def __init__(self, hd_active: HdActive, parent: QWidget):
         super().__init__(parent)
         self.hd_active = hd_active
-        self.ui = Ui_LogDialog()
-        self.ui.setupUi(self)
+        self.setupUi(self)
 
         # UI bindings
-        self.ui.ok_button.clicked.connect(self.close)
+        self.ok_button.clicked.connect(self.close)
 
     def showEvent(self, arg__1: QShowEvent):
         super().showEvent(arg__1)
@@ -22,4 +20,4 @@ class LogDialog(QDialog):
         self.set_text()
 
     def set_text(self):
-        self.ui.log_textBrowser.setText('\n'.join(self.hd_active.log))
+        self.log_textBrowser.setText('\n'.join(self.hd_active.log))
