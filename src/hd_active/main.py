9282b864e5ff1a59f051d20689df7691ae14fd31
Joao Coelho <joaonc@hotmail.com>
2025-09-01 20:41:47 -0500

    fix

--- src/hd_active/main.py
+++ src/hd_active/main.py
@@ -1,8 +1,9 @@
 import sys
+from pathlib import Path
 
 from PySide6 import QtGui, QtWidgets
 
-from .ui.system_tray_icon import SystemTrayIcon
+from .ui.system_tray_icon import DEFAULT_HD_ACTIVE_FILE_NAME, SystemTrayIcon
 from .utils import get_asset
 
 
@@ -12,10 +13,17 @@ def main():
     """
     app = QtWidgets.QApplication(sys.argv)
     widget = QtWidgets.QWidget()
+    hd_active_file_name = (
+        Path(sys.argv[1]).expanduser().resolve()
+        if len(sys.argv) > 1
+        else DEFAULT_HD_ACTIVE_FILE_NAME
+    )
+
     tray_icon = SystemTrayIcon(
         # Icon from https://icon-icons.com/icon/drive-harddisk-usb/36212 (GPL v3)
         icon=QtGui.QIcon(str(get_asset('images', 'hard_drive_icon.png'))),
         parent=widget,
+        hd_active_file_name=hd_active_file_name,
     )
     tray_icon.show()
     tray_icon.showMessage('HD Active started', 'The message')
