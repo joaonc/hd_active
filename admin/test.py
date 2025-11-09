6da76d2c77e2e26cc30b41e1708a508666b831c0
Joao Coelho <joaonc@hotmail.com>
2025-11-09 14:02:03 -0600

    admin scripts

---
+++ admin/test.py
@@ -0,0 +1,29 @@
+#!python
+"""
+Testing with `pytest`.
+"""
+
+import typer
+
+from admin.utils import DryAnnotation, run
+
+app = typer.Typer(
+    help=__doc__,
+    no_args_is_help=True,
+    add_completion=False,
+    rich_markup_mode='markdown',
+)
+
+
+@app.command(name='unit')
+def test_unit(dry: DryAnnotation = False):
+    """
+    Run unit tests.
+
+    Unit test configuration in ``pyproject.toml``.
+    """
+    run(dry, 'pytest', '.')
+
+
+if __name__ == '__main__':
+    app()
